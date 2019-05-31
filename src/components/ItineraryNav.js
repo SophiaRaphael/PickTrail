8624c5819b440b9c1d56a7e8994a352cf1810fb2
sophia_raphael <sophia.raphael@weboniselab.com>
2019-05-31 18:06:21 +0530

    Make the Group-Navigation sticky to top once out of viewport

--- src/components/ItineraryNav.js
+++ src/components/ItineraryNav.js
@@ -1,16 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../styles/itinerary.sass';
 
-const ItineraryNav = ({groupList}) => (
-  <ul className='itineraryList'>
-    {groupList.map((groupName, key) =>
-      <li key={key}>
-        <a href={'#'+groupName}> 
-          {groupName} 
-        </a>
-      </li>
-    )}
-  </ul>
-);
+class ItineraryNav extends Component { 
+  constructor(props) {
+    super(props);
+    this.state = {};
+		this.handleScroll = this.handleScroll.bind(this);
+	}
+
+	handleScroll() { 
+    const {navElement} = this.state;
+    this.setState({ 
+      top: navElement.offsetTop,  
+      scroll: window.scrollY
+    });
+  }
+  
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+    const navElement = document.querySelector('.itineraryBox');
+    this.setState({
+      navElement: navElement
+    })
+	}
+  
+  render() {
+    const {groupList} = this.props;
+
+    return(
+      <ul className={this.state.scroll > this.state.top ? "fixedNav itineraryList" : "itineraryList"}>
+        {groupList.map((groupName, key) =>
+          <li key={key}>
+            <a href={'#'+groupName}> 
+              {groupName} 
+            </a>
+          </li>
+        )}
+      </ul>
+    );
+  }
+}
 
 export default ItineraryNav;
